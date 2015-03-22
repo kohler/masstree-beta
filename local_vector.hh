1150824e6a10a0384ba4bfed6b916ce62f96315f
Eddie Kohler <ekohler@gmail.com>
2015-03-22 09:35:08 -0400

    Update local_vector.
    Wish this didn't make local_vector bigger on 64-bit but it does.

--- local_vector.hh
+++ local_vector.hh
@@ -3,13 +3,15 @@
 #include "compiler.hh"
 #include <memory>
 #include <iterator>
+#include <assert.h>
 
 template <typename T, int N, typename A = std::allocator<T> >
 class local_vector {
   public:
     typedef bool (local_vector<T, N, A>::*unspecified_bool_type)() const;
-    typedef T* iterator;
-    typedef const T* const_iterator;
+    typedef T value_type;
+    typedef value_type* iterator;
+    typedef const value_type* const_iterator;
     typedef std::reverse_iterator<iterator> reverse_iterator;
     typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
     typedef unsigned size_type;
@@ -21,6 +23,7 @@ class local_vector {
     inline ~local_vector();
 
     inline size_type size() const;
+    inline size_type capacity() const;
     inline bool empty() const;
     inline operator unspecified_bool_type() const;
     inline bool operator!() const;
@@ -38,18 +41,22 @@ class local_vector {
     inline const_reverse_iterator crbegin() const;
     inline const_reverse_iterator crend() const;
 
-    inline T& operator[](size_type i);
-    inline const T& operator[](size_type i) const;
-    inline T& front();
-    inline const T& front() const;
-    inline T& back();
-    inline const T& back() const;
+    inline value_type& operator[](size_type i);
+    inline const value_type& operator[](size_type i) const;
+    inline value_type& front();
+    inline const value_type& front() const;
+    inline value_type& back();
+    inline const value_type& back() const;
 
-    inline void clear();
-    inline void push_back(const T& x);
-    inline void push_back(T&& x);
+    inline void push_back(const value_type& x);
+    inline void push_back(value_type&& x);
     template <typename... Args> inline void emplace_back(Args&&... args);
     inline void pop_back();
+
+    inline void clear();
+    inline void resize(size_type n, value_type x = value_type());
+    iterator erase(iterator position);
+    iterator erase(iterator first, iterator last);
 
     inline local_vector<T, N, A>& operator=(const local_vector<T, N, A>& x);
     template <int NN, typename AA>
@@ -57,21 +64,22 @@ class local_vector {
 
   private:
     struct rep : public A {
-        T* v_;
-        size_type size_;
-        size_type capacity_;
-        char lv_[sizeof(T) * N];
+        T* first_;
+        T* last_;
+        T* capacity_;
+        char lv_[sizeof(T) * N]; // XXX does not obey alignof(T)
 
         inline rep(const A& a);
     };
     rep r_;
 
-    void grow();
+    void grow(size_type n = 0);
 };
 
 template <typename T, int N, typename A>
 inline local_vector<T, N, A>::rep::rep(const A& a)
-    : A(a), v_(reinterpret_cast<T*>(lv_)), size_(0), capacity_(N) {
+    : A(a), first_(reinterpret_cast<T*>(lv_)),
+      last_(first_), capacity_(first_ + N) {
 }
 
 template <typename T, int N, typename A>
@@ -82,33 +90,38 @@ inline local_vector<T, N, A>::local_vector(const A& allocator)
 template <typename T, int N, typename A>
 local_vector<T, N, A>::local_vector(const local_vector<T, N, A>& x)
     : r_(A()) {
-    for (size_type i = 0; i != x.r_.size_; ++i)
-        push_back(x.r_.v_[i]);
+    for (const T* it = x.r_.first_; it != x.r_.last_; ++it)
+        push_back(*it);
 }
 
 template <typename T, int N, typename A> template <int NN, typename AA>
 local_vector<T, N, A>::local_vector(const local_vector<T, NN, AA>& x)
     : r_(A()) {
-    for (size_type i = 0; i != x.r_.size_; ++i)
-        push_back(x.r_.v_[i]);
+    for (const T* it = x.r_.first_; it != x.r_.last_; ++it)
+        push_back(*it);
 }
 
 template <typename T, int N, typename A>
 inline local_vector<T, N, A>::~local_vector() {
-    for (size_type i = 0; i != r_.size_; ++i)
-        r_.destroy(&r_.v_[i]);
-    if (r_.v_ != reinterpret_cast<T*>(r_.lv_))
-        r_.deallocate(r_.v_, r_.capacity_);
+    for (T* it = r_.first_; it != r_.last_; ++it)
+        r_.destroy(it);
+    if (r_.first_ != reinterpret_cast<T*>(r_.lv_))
+        r_.deallocate(r_.first_, r_.capacity_ - r_.first_);
 }
 
 template <typename T, int N, typename A>
 inline unsigned local_vector<T, N, A>::size() const {
-    return r_.size_;
+    return r_.last_ - r_.first_;
+}
+
+template <typename T, int N, typename A>
+inline unsigned local_vector<T, N, A>::capacity() const {
+    return r_.capacity_ - r_.first_;
 }
 
 template <typename T, int N, typename A>
 inline bool local_vector<T, N, A>::empty() const {
-    return r_.size_ == 0;
+    return r_.first_ == r_.last_;
 }
 
 template <typename T, int N, typename A>
@@ -122,46 +135,50 @@ inline bool local_vector<T, N, A>::operator!() const {
 }
 
 template <typename T, int N, typename A>
-void local_vector<T, N, A>::grow() {
-    T* m = r_.allocate(r_.capacity_ * 2);
-    for (size_type i = 0; i != r_.size_; ++i) {
-        r_.construct(&m[i], std::move(r_.v_[i]));
-        r_.destroy(&r_.v_[i]);
+void local_vector<T, N, A>::grow(size_type n) {
+    size_t newcap = capacity() * 2;
+    while (newcap < n)
+        newcap *= 2;
+    T* m = r_.allocate(newcap);
+    for (T* it = r_.first_, *mit = m; it != r_.last_; ++it, ++mit) {
+        r_.construct(mit, std::move(*it));
+        r_.destroy(it);
     }
-    if (r_.v_ != reinterpret_cast<T*>(r_.lv_))
-        r_.deallocate(r_.v_, r_.capacity_);
-    r_.v_ = m;
-    r_.capacity_ *= 2;
+    if (r_.first_ != reinterpret_cast<T*>(r_.lv_))
+        r_.deallocate(r_.first_, capacity());
+    r_.last_ = m + (r_.last_ - r_.first_);
+    r_.first_ = m;
+    r_.capacity_ = m + newcap;
 }
 
 template <typename T, int N, typename A>
 inline auto local_vector<T, N, A>::begin() -> iterator {
-    return r_.v_;
+    return r_.first_;
 }
 
 template <typename T, int N, typename A>
 inline auto local_vector<T, N, A>::end() -> iterator {
-    return r_.v_ + r_.size_;
+    return r_.last_;
 }
 
 template <typename T, int N, typename A>
 inline auto local_vector<T, N, A>::begin() const -> const_iterator {
-    return r_.v_;
+    return r_.first_;
 }
 
 template <typename T, int N, typename A>
 inline auto local_vector<T, N, A>::end() const -> const_iterator {
-    return r_.v_ + r_.size_;
+    return r_.last_;
 }
 
 template <typename T, int N, typename A>
 inline auto local_vector<T, N, A>::cbegin() const -> const_iterator {
-    return r_.v_;
+    return r_.first_;
 }
 
 template <typename T, int N, typename A>
 inline auto local_vector<T, N, A>::cend() const -> const_iterator {
-    return r_.v_ + r_.size_;
+    return r_.last_;
 }
 
 template <typename T, int N, typename A>
@@ -196,70 +213,83 @@ inline auto local_vector<T, N, A>::crend() const -> const_reverse_iterator {
 
 template <typename T, int N, typename A>
 inline T& local_vector<T, N, A>::operator[](size_type i) {
-    return r_.v_[i];
+    return r_.first_[i];
 }
 
 template <typename T, int N, typename A>
 inline const T& local_vector<T, N, A>::operator[](size_type i) const {
-    return r_.v_[i];
+    return r_.first_[i];
 }
 
 template <typename T, int N, typename A>
 inline T& local_vector<T, N, A>::front() {
-    return r_.v_[0];
+    return r_.first_[0];
 }
 
 template <typename T, int N, typename A>
 inline const T& local_vector<T, N, A>::front() const {
-    return r_.v_[0];
+    return r_.first_[0];
 }
 
 template <typename T, int N, typename A>
 inline T& local_vector<T, N, A>::back() {
-    return r_.v_[r_.size_ - 1];
+    return r_.last_[-1];
 }
 
 template <typename T, int N, typename A>
 inline const T& local_vector<T, N, A>::back() const {
-    return r_.v_[r_.size_ - 1];
+    return r_.last_[-1];
 }
 
 template <typename T, int N, typename A>
 inline void local_vector<T, N, A>::push_back(const T& x) {
-    if (r_.size_ == r_.capacity_)
+    if (r_.last_ == r_.capacity_)
         grow();
-    r_.construct(&r_.v_[r_.size_], x);
-    ++r_.size_;
+    r_.construct(r_.last_, x);
+    ++r_.last_;
 }
 
 template <typename T, int N, typename A>
 inline void local_vector<T, N, A>::push_back(T&& x) {
-    if (r_.size_ == r_.capacity_)
+    if (r_.last_ == r_.capacity_)
         grow();
-    r_.construct(&r_.v_[r_.size_], std::move(x));
-    ++r_.size_;
+    r_.construct(r_.last_, std::move(x));
+    ++r_.last_;
 }
 
 template <typename T, int N, typename A> template <typename... Args>
 inline void local_vector<T, N, A>::emplace_back(Args&&... args) {
-    if (r_.size_ == r_.capacity_)
+    if (r_.last_ == r_.capacity_)
         grow();
-    r_.construct(&r_.v_[r_.size_], std::forward<Args>(args)...);
-    ++r_.size_;
+    r_.construct(r_.last_, std::forward<Args>(args)...);
+    ++r_.last_;
 }
 
 template <typename T, int N, typename A>
 inline void local_vector<T, N, A>::pop_back() {
-    assert(r_.size_ != 0);
-    --r_.size_;
-    r_.destroy(&r_.v_[r_.size_]);
+    assert(r_.first_ != r_.last_);
+    --r_.last_;
+    r_.destroy(r_.last_);
 }
 
 template <typename T, int N, typename A>
 inline void local_vector<T, N, A>::clear() {
-    for (size_type i = 0; i != r_.size_; ++i)
-        r_.destroy(&r_.v_[i]);
-    r_.size_ = 0;
+    for (auto it = r_.first_; it != r_.last_; ++it)
+        r_.destroy(it);
+    r_.last_ = r_.first_;
+}
+
+template <typename T, int N, typename A>
+inline void local_vector<T, N, A>::resize(size_type n, value_type v) {
+    if (capacity() < n)
+        grow(n);
+    auto it = r_.first_ + n;
+    auto xt = r_.last_;
+    r_.last_ = it;
+    for (; it < xt; ++it)
+        r_.destroy(it);
+    for (; xt < it; ++xt)
+        r_.construct(xt, v);
 }
 
 template <typename T, int N, typename A>
@@ -267,8 +297,10 @@ local_vector<T, N, A>&
 local_vector<T, N, A>::operator=(const local_vector<T, N, A>& x) {
     if (&x != this) {
         clear();
-        for (size_type i = 0; i != x.r_.size_; ++i)
-            push_back(x.r_.v_[i]);
+        if (capacity() < x.capacity())
+            grow(x.capacity());
+        for (auto xit = x.r_.first_; xit != x.r_.last_; ++xit, ++r_.last_)
+            r_.construct(r_.last_, *xit);
     }
     return *this;
 }
@@ -277,9 +309,29 @@ template <typename T, int N, typename A> template <int NN, typename AA>
 local_vector<T, N, A>&
 local_vector<T, N, A>::operator=(const local_vector<T, NN, AA>& x) {
     clear();
-    for (size_type i = 0; i != x.r_.size_; ++i)
-        push_back(x.r_.v_[i]);
+    if (capacity() < x.capacity())
+        grow(x.capacity());
+    for (auto xit = x.r_.first_; xit != x.r_.last_; ++xit, ++r_.last_)
+        r_.construct(r_.last_, *xit);
     return *this;
+}
+
+template <typename T, int N, typename A>
+inline T* local_vector<T, N, A>::erase(iterator position) {
+    return erase(position, position + 1);
+}
+
+template <typename T, int N, typename A>
+T* local_vector<T, N, A>::erase(iterator first, iterator last) {
+    if (first != last) {
+        iterator it = first, xend = end();
+        for (; last != xend; ++it, ++last)
+            *it = std::move(*last);
+        r_.last_ = it;
+        for (; it != xend; ++it)
+            r_.destroy(it);
+    }
+    return first;
 }
 
 #endif
